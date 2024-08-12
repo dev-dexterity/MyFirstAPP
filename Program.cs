b36dfdd8a95620dbb26423bb1fc3a87e0a06b9ea
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-12 20:56:46 +0530

    simple program of variables

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        int num = 2;
+        float price = 3.99f;
+
+        Console.WriteLine(price * num);
     }
-}
+}   
