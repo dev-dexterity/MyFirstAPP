28c35e1020dba32f6180584128997d396e6606e3
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-12 21:31:10 +0530

    just made a little changes

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
-﻿using System.Data;
+﻿using System;
+
+
 namespace MyFirstAPP
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            /*int a = 1;
-            int b = 2;
-
-            a = 5;
-
-            int c = a + b;
-            Console.WriteLine("The Addition gives = " + c);
-
-            Console.WriteLine("Thank You!");*/
             MyFirstAPP.Variables.Var();
+            Console.WriteLine("Thank You!");
 
         }
     }
