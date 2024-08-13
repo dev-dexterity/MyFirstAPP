b1edfc8197f138c40cc7a0c3ed448d7e305ec9f3
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-14 00:01:23 +0530

    Made Some Major Formatting.

--- CatchingErrors.cs
+++ CatchingErrors.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Threading.Channels;
+
+namespace MyFirstAPP
+{
+    public class TryCatchErrors
+    {
+        public static void ErrorHandling()
+        {
+            Console.WriteLine("Please Enter a number");
+            string userInput = Console.ReadLine();
+
+            int userInputAsInt = int.Parse(userInput);
+
+            Console.WriteLine(userInputAsInt);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,25 +10,30 @@ namespace MyFirstAPP
         /// </summary>
         public static void Main(string[] args)
         {
-            /*MyFirstAPP.Variables.Var();
-            MyFirstAPP.SwapVariables.Swapping();
-            Console.WriteLine(MyFirstAPP.Methods.Subtract(a, b));
-            Console.WriteLine(MyFirstAPP.Methods.Multiply(a, b));
-            Console.WriteLine(MyFirstAPP.Methods.Divide(a, b));
+            /*
+             * Variables and Datatypes
+             * MyFirstAPP.Variables.Var();
+             * MyFirstAPP.SwapVariables.Swapping();
+             
+             * Methods
+             * Console.Write("Your Desired output is: " + MyFirstAPP.Methods.Add(num1, num2));
+             * Console.WriteLine(MyFirstAPP.Methods.Subtract(a, b));
+             * Console.WriteLine(MyFirstAPP.Methods.Multiply(a, b));
+             * Console.WriteLine(MyFirstAPP.Methods.Divide(a, b));
+             
+             * UserInput
+             * MyFirstAPP.UserInput.UserInputs();
+             * Console.Write("Type Your first number: ");
+             * string num1Input = Console.ReadLine();
+             * Console.Write("Type Your Second number: ");
+             * string num2Input = Console.ReadLine();
+             * int num1 = int.Parse(num1Input);
+             * int num2 = int.Parse(num2Input);//parsing  is effective if you have specific input
+             */
 
-            MyFirstAPP.UserInput.UserInputs();*/
-            Console.Write("Type Your first number: ");
-            string num1Input = Console.ReadLine();
+            //Catching Errors
+            MyFirstAPP.TryCatchErrors.ErrorHandling();
 
-            Console.Write("Type Your Second number: ");
-            string num2Input = Console.ReadLine();
-
-            int num1 = int.Parse(num1Input);
-            int num2 = int.Parse(num2Input);//parsing  is effective if you have specific input
-
-
-            Console.Write("Your Desired output is: " + MyFirstAPP.Methods.Add(num1, num2));
-      
             Console.WriteLine();
             Console.WriteLine("Thank You!");
         }
