59f966389ed5cf242f46e72bc4bf6b4ccd50bfa3
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-13 23:53:06 +0530

    I have leart user input and what is type casting or rather parsing.It is an process to convert a string to integer(parsing). The process of converting any datatype to which ever we want it called type casting. Programming is FUN!!!

---
+++ UserInput.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstAPP
+{
+    public class UserInput
+    {
+        public static void UserInputs()
+        {
+            Console.Write("Type Something: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
