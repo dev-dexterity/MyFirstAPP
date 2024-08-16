3d671f5bcaa24e09d1bdb27378d6954569505325
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-16 22:55:16 +0530

    Just organized the project directory

---
+++ OOP_Concepts/Properties.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstAPP
+{
+    class Box
+    {
+        public int length;
+        public int height;
+        public int width;
+        public int volume;
+
+
+
+    }
+}
