41d5de0f8471130c5ba1fa05a99020c645781bba
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-22 22:10:19 +0530

    just some minor changes

--- Arrays.cs
+++ Arrays.cs
@@ -8,18 +8,19 @@ namespace MyFirstAPP
     {
         public static void ArrayLecture()
         {
-            int[] arr = new int[10];
-
+            int[] arr = new int[] {1,2,3,4,2,3,2,3,5,1};
+            Console.WriteLine("Before changing [{0}]", string.Join(",", arr));
+            
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = i + 1;
                 
             }
-            Console.WriteLine("[{0}]", string.Join(",", arr));
+            Console.WriteLine("After changing [{0}]", string.Join(",", arr));
 
             int input = Convert.ToInt32(Console.ReadLine());
             arr[0] = input;
-            Console.WriteLine("[{0}]", string.Join(",", arr));
+            Console.WriteLine("After changing only one value [{0}]", string.Join(",", arr));
             Console.WriteLine("array value at index 0 changed to : {0}", arr[0]);
 
         }
