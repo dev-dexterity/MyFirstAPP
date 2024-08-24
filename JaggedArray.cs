1852de6ce085a48117bbc843d761cbc74024ad40
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-24 14:48:52 +0530

    Done with jagged arrays. Programming is FUN!!!

--- JaggedArray.cs
+++ JaggedArray.cs
@@ -4,6 +4,49 @@
     {
         public static void JaggedArrays()
         {
+            //declaration of jagged array
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[5];
+            jaggedArray [1] = new int[3];
+            jaggedArray[2] = new int[2];
+
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1, 2, 3 };
+            jaggedArray[2] = new int[] { 13, 21 };
+
+            //alternative way of declaring jagged array
+            int[][] jaggedArray2 = new int[][]
+            {
+                new int[] { 2, 3, 5, 7, 11 },
+                new int[] { 1, 2, 3 }
+            };
+
+           
+
+            for(int i = 0; i < jaggedArray2.Length; i++)
+            {
+                Console.WriteLine("Element {0}", i + 1);
+                for (int j = 0; j < jaggedArray2[i].Length; j++)
+                {
+                    Console.Write("{0} ", jaggedArray2[i][j]);
+                    
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.WriteLine("Element {0}", i + 1);
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write("{0} ", jaggedArray[i][j]);
+                }
+                Console.WriteLine("\n");
+            }
+
 
         }
     }
